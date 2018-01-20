35e295bceb84c6f6c84dd9413ca4d478743fb0b3
taobig <studentht@hotmail.com>
2018-01-20 15:53:08 +0800

    update StringEncodingHelperTest

--- tests/helpers/StringEncodingHelperTest.php
+++ tests/helpers/StringEncodingHelperTest.php
@@ -10,6 +10,19 @@ class StringEncodingHelperTest extends TestCase
         $str = "\u4f60\u597d";
         $unicodeStr = (new StringEncodingHelper())->decodeUnicode($str);
         $this->assertSame("你好", $unicodeStr);
+
+        $str = "";
+        $unicodeStr = (new StringEncodingHelper())->decodeUnicode($str);
+        $this->assertSame("", $unicodeStr);
+
+
+        $str = " ";
+        $unicodeStr = (new StringEncodingHelper())->decodeUnicode($str);
+        $this->assertSame(" ", $unicodeStr);
+
+        $str = "[\"\u4f60\u597d\", [\"\u4f60\u597d\"]";
+        $unicodeStr = (new StringEncodingHelper())->decodeUnicode($str);
+        $this->assertSame("[\"你好\", [\"你好\"]", $unicodeStr);
     }
 
 }
