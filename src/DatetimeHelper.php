a6afe3e1d767fc6901644864bc91ffbf3d6d2aa1
taobig <studentht@hotmail.com>
2018-10-10 19:13:31 +0800

    add DatetimeHelper::datetimeDiff

--- src/DatetimeHelper.php
+++ src/DatetimeHelper.php
@@ -19,4 +19,19 @@ class DatetimeHelper
     {
         return date("Y-m-d H:i:s");
     }
+
+    /**
+     * this method need 64-bit PHP runtime.(32-bit时只能表示67年的时间差)
+     * @param string $strDtLeft
+     * @param string $strDtRight
+     * @return int
+     */
+    public static function datetimeDiff(string $strDtLeft, string $strDtRight): int
+    {
+        $dtLeft = new \DateTime($strDtLeft);
+        $dtRight = new \DateTime($strDtRight);
+
+        return $dtLeft->getTimestamp() - $dtRight->getTimestamp();
+    }
+
 }
--- tests/helpers/DatetimeHelperTest.php
+++ tests/helpers/DatetimeHelperTest.php
@@ -13,4 +13,18 @@ class DatetimeHelperTest extends TestCase
         $this->assertSame(true, $time - 1 == $second || $second == $time);
     }
 
+    public function testDatetimeDiff()
+    {
+        $this->assertSame(0, DatetimeHelper::datetimeDiff("2018-10-10 00:00:00", "2018-10-10 00:00:00"));
+        $this->assertSame(1, DatetimeHelper::datetimeDiff("2018-10-10 00:00:01", "2018-10-10 00:00:00"));
+        $this->assertSame(-1, DatetimeHelper::datetimeDiff("2018-10-10 00:00:00", "2018-10-10 00:00:01"));
+
+        $this->assertSame(-1, DatetimeHelper::datetimeDiff("20181010000000", "20181010000001"));
+
+        $this->assertSame(86400, DatetimeHelper::datetimeDiff("20181011", "20181010"));
+        $this->assertSame(0, DatetimeHelper::datetimeDiff("20181010", "20181010"));
+
+        $this->assertSame(315572198400, DatetimeHelper::datetimeDiff("99991231", "00000000"));
+    }
+
 }
