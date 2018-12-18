ed505ce3569874381d98e2e329bf2f2bf1dd4ef8
taobig <studentht@hotmail.com>
2018-12-18 11:35:11 +0800

    add UploadedFile

---
+++ src/utils/UploadedFile.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace taobig\helpers\utils;
+
+/**
+ * Based on Yii2 yii\web\UploadedFile class
+ * @see https://www.yiiframework.com/doc/api/2.0/yii-web-uploadedfile
+ */
+class UploadedFile
+{
+
+    /**
+     * @var string the original name of the file being uploaded
+     */
+    public $name;
+    /**
+     * @var string the path of the uploaded file on the server.
+     * Note, this is a temporary file which will be automatically deleted by PHP
+     * after the current request is processed.
+     */
+    public $tempName;
+    /**
+     * @var string the MIME-type of the uploaded file (such as "image/gif").
+     * Since this MIME type is not checked on the server-side, do not take this value for granted.
+     * Instead, use [[\yii\helpers\FileHelper::getMimeType()]] to determine the exact MIME type.
+     */
+    public $type;
+    /**
+     * @var int the actual size of the uploaded file in bytes
+     */
+    public $size;
+    /**
+     * @var int an error code describing the status of this file uploading.
+     * @see http://www.php.net/manual/en/features.file-upload.errors.php
+     */
+    public $error;
+
+    /**
+     * @var int[]
+     */
+    private static $errors = [
+        UPLOAD_ERR_OK,
+        UPLOAD_ERR_INI_SIZE,
+        UPLOAD_ERR_FORM_SIZE,
+        UPLOAD_ERR_PARTIAL,
+        UPLOAD_ERR_NO_FILE,
+        UPLOAD_ERR_NO_TMP_DIR,
+        UPLOAD_ERR_CANT_WRITE,
+        UPLOAD_ERR_EXTENSION,
+    ];
+
+    private static $_files;
+
+
+    public function __construct($properties = [])
+    {
+        if (!empty($properties)) {
+            foreach ($properties as $name => $value) {
+                $this->$name = $value;
+            }
+        }
+    }
+
+
+    /**
+     * Returns an uploaded file according to the given file input name.
+     * The name can be a plain string or a string like an array element (e.g. 'Post[imageFile]', or 'Post[0][imageFile]').
+     * @param string $name the name of the file input field.
+     * @return null|UploadedFile the instance of the uploaded file.
+     * Null is returned if no file is uploaded for the specified name.
+     */
+    public static function getInstanceByName($name)
+    {
+        $files = self::loadFiles();
+        return isset($files[$name]) ? new static($files[$name]) : null;
+    }
+
+    /**
+     * Returns an array of uploaded files corresponding to the specified file input name.
+     * This is mainly used when multiple files were uploaded and saved as 'files[0]', 'files[1]',
+     * 'files[n]'..., and you can retrieve them all by passing 'files' as the name.
+     * @param string $name the name of the array of files
+     * @return UploadedFile[] the array of UploadedFile objects. Empty array is returned
+     * if no adequate upload was found. Please note that this array will contain
+     * all files from all sub-arrays regardless how deeply nested they are.
+     */
+    public static function getInstancesByName($name)
+    {
+        $files = self::loadFiles();
+        if (isset($files[$name])) {
+            return [new static($files[$name])];
+        }
+        $results = [];
+        foreach ($files as $key => $file) {
+            if (strpos($key, "{$name}[") === 0) {
+                $results[] = new static($file);
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * @return UploadedFile[]
+     */
+    public static function getInstances()
+    {
+        $files = self::loadFiles();
+        $results = [];
+        foreach ($files as $key => $file) {
+            $results[] = new static($file);
+        }
+
+        return $results;
+    }
+
+    /**
+     * Cleans up the loaded UploadedFile instances.
+     * This method is mainly used by test scripts to set up a fixture.
+     */
+    public static function reset()
+    {
+        self::$_files = null;
+    }
+
+    /**
+     * Saves the uploaded file.
+     * Note that this method uses php's move_uploaded_file() method. If the target file `$file`
+     * already exists, it will be overwritten.
+     * @param string $file the file path used to save the uploaded file
+     * @param bool $deleteTempFile whether to delete the temporary file after saving.
+     * If true, you will not be able to save the uploaded file again in the current request.
+     * @return bool true whether the file is saved successfully
+     * @see error
+     */
+    public function saveAs($file, $deleteTempFile = true)
+    {
+        if ($this->error == UPLOAD_ERR_OK) {
+            if ($deleteTempFile) {
+                return move_uploaded_file($this->tempName, $file);
+            } elseif (is_uploaded_file($this->tempName)) {
+                return copy($this->tempName, $file);
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string original file base name
+     */
+    public function getBaseName()
+    {
+        // https://github.com/yiisoft/yii2/issues/11012
+        $pathInfo = pathinfo('_' . $this->name, PATHINFO_FILENAME);
+        return mb_substr($pathInfo, 1, mb_strlen($pathInfo, '8bit'), '8bit');
+    }
+
+    /**
+     * @return string file extension
+     */
+    public function getExtension()
+    {
+        return strtolower(pathinfo($this->name, PATHINFO_EXTENSION));
+    }
+
+    /**
+     * @return bool whether there is an error with the uploaded file.
+     * Check [[error]] for detailed error code information.
+     */
+    public function getHasError()
+    {
+        return $this->error != UPLOAD_ERR_OK;
+    }
+
+    /**
+     * Creates UploadedFile instances from $_FILE.
+     * @return array the UploadedFile instances
+     */
+    private static function loadFiles()
+    {
+        if (self::$_files === null) {
+            self::$_files = [];
+            if (isset($_FILES) && is_array($_FILES)) {
+                foreach ($_FILES as $class => $info) {
+                    self::loadFilesRecursive($class, $info['name'], $info['tmp_name'], $info['type'], $info['size'], $info['error']);
+                }
+            }
+        }
+
+        return self::$_files;
+    }
+
+    /**
+     * Creates UploadedFile instances from $_FILE recursively.
+     * @param string $key key for identifying uploaded file: class name and sub-array indexes
+     * @param mixed $names file names provided by PHP
+     * @param mixed $tempNames temporary file names provided by PHP
+     * @param mixed $types file types provided by PHP
+     * @param mixed $sizes file sizes provided by PHP
+     * @param mixed $errors uploading issues provided by PHP
+     */
+    private static function loadFilesRecursive($key, $names, $tempNames, $types, $sizes, $errors)
+    {
+        if (is_array($names)) {
+            foreach ($names as $i => $name) {
+                self::loadFilesRecursive($key . '[' . $i . ']', $name, $tempNames[$i], $types[$i], $sizes[$i], $errors[$i]);
+            }
+        } elseif ((int)$errors !== UPLOAD_ERR_NO_FILE) {
+            self::$_files[$key] = [
+                'name' => $names,
+                'tempName' => $tempNames,
+                'type' => $types,
+                'size' => $sizes,
+                'error' => $errors,
+            ];
+        }
+    }
+}
