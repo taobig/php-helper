3318b43bbdc349de1e8ae22ff343b0594875cdb5
taobig <studentht@hotmail.com>
2019-07-31 18:24:24 +0800

    update DatetimeHelper

--- src/DatetimeHelper.php
+++ src/DatetimeHelper.php
@@ -26,6 +26,40 @@ class DatetimeHelper
     }
 
     /**
+     * @param int|string|\DateTime $referenceTime   time() or "2019-07-31" or \DateTime
+     * @return false|string
+     */
+    public static function getYesterdayDate($referenceTime = null)
+    {
+        $time = time();
+        if (is_int($referenceTime)) {
+            $time = $referenceTime;
+        } else if (is_string($referenceTime)) {
+            $time = strtotime($referenceTime);
+        }else if($referenceTime instanceof \DateTime){
+            $time = $referenceTime->getTimestamp();
+        }
+        return date("Y-m-d", strtotime("-1 days", $time));
+    }
+
+    /**
+     * @param int|string|\DateTime $referenceTime   time() or "2019-07-31" or \DateTime
+     * @return false|string
+     */
+    public static function getTomorrowDate($referenceTime = null)
+    {
+        $time = time();
+        if (is_int($referenceTime)) {
+            $time = $referenceTime;
+        } else if (is_string($referenceTime)) {
+            $time = strtotime($referenceTime);
+        }else if($referenceTime instanceof \DateTime){
+            $time = $referenceTime->getTimestamp();
+        }
+        return date("Y-m-d", strtotime("+1 days", $time));
+    }
+
+    /**
      * this method need 64-bit PHP runtime.(32-bit时只能表示67年的时间差)
      * @param string $strDtLeft
      * @param string $strDtRight
--- tests/helpers/DatetimeHelperTest.php
+++ tests/helpers/DatetimeHelperTest.php
@@ -42,5 +42,39 @@ class DatetimeHelperTest extends TestCase
         $this->assertRegExp("/^[2-9][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dt);
     }
 
+    public function testGetYesterdayDate()
+    {
+        $dt = DatetimeHelper::getYesterdayDate();
+        $this->assertSame(date('Y-m-d', time() - 24 * 3600), $dt);
+
+        $dt = DatetimeHelper::getYesterdayDate('2019-07-31');
+        $this->assertSame('2019-07-30', $dt);
+
+        $dt = DatetimeHelper::getYesterdayDate(strtotime('2019-07-31'));
+        $this->assertSame('2019-07-30', $dt);
+
+        $date = new DateTime();
+        $date->setTimestamp(strtotime('2019-07-31'));
+        $dt = DatetimeHelper::getYesterdayDate($date);
+        $this->assertSame('2019-07-30', $dt);
+    }
+
+    public function testGetTomorrowDate()
+    {
+        $dt = DatetimeHelper::getTomorrowDate();
+        $this->assertSame(date('Y-m-d', time() + 24 * 3600), $dt);
+
+        $dt = DatetimeHelper::getTomorrowDate('2019-07-31');
+        $this->assertSame('2019-08-01', $dt);
+
+        $dt = DatetimeHelper::getTomorrowDate(strtotime('2019-07-31'));
+        $this->assertSame('2019-08-01', $dt);
+
+        $date = new DateTime();
+        $date->setTimestamp(strtotime('2019-07-31'));
+        $dt = DatetimeHelper::getTomorrowDate($date);
+        $this->assertSame('2019-08-01', $dt);
+    }
+
 
 }
