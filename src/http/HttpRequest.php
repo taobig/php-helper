e85311d09baeb603f67196b87e1f26c71105f92d
taobig <studentht@hotmail.com>
2020-01-11 12:04:13 +0800

    update HttpRequest

--- src/http/HttpRequest.php
+++ src/http/HttpRequest.php
@@ -30,22 +30,22 @@ class HttpRequest
 
     }
 
-    public function postJson(string $url, array $params = [], float $timeout = 1.0)
+    public function postJson(string $url, array $params = [], float $timeout = 1.0, array $headers = [])
     {
-        return self::post($url, $params, 'application/json', $timeout);
+        return self::post($url, $params, 'application/json', $timeout, $headers);
     }
 
-    public function postForm(string $url, array $params = [], float $timeout = 1.0)
+    public function postForm(string $url, array $params = [], float $timeout = 1.0, array $headers = [])
     {
-        return self::post($url, $params, 'application/x-www-form-urlencoded', $timeout);
+        return self::post($url, $params, 'application/x-www-form-urlencoded', $timeout, $headers);
     }
 
-    public function postFile(string $url, array $params = [], float $timeout = 1.0)
+    public function postFile(string $url, array $params = [], float $timeout = 1.0, array $headers = [])
     {
-        return self::post($url, $params, 'multipart/form-data', $timeout);
+        return self::post($url, $params, 'multipart/form-data', $timeout, $headers);
     }
 
-    private function post(string $url, array $params, string $contentType, float $timeout)
+    private function post(string $url, array $params, string $contentType, float $timeout, array $headers = []): string
     {
         $client = new Client([
             // Base URI is used with relative requests
@@ -60,9 +60,11 @@ class HttpRequest
         } else if ($contentType === 'multipart/form-data') {//docs: http://docs.guzzlephp.org/en/stable/request-options.html#multipart
             $option ['multipart'] = $params;
         }
+        if ($headers) {
+            $option['headers'] = $headers;
+        }
         $response = $client->request('POST', $url, $option);
-        $content = $response->getBody()->getContents();
-        return $content;
+        return $response->getBody()->getContents();
     }
 
 }
