cdce10ddf4ad9dd65b6105146a8e2990289f522c
taobig <studentht@hotmail.com>
2020-03-17 22:14:51 +0800

    add EnumBase

---
+++ src/base/EnumBase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace taobig\helpers\base;
+
+
+abstract class EnumBase
+{
+
+    abstract public static function labels(): array;
+
+    public static function dropdownList($defaultValue = null, string $name = ''): array
+    {
+        if ($defaultValue !== null) {
+            $result[$defaultValue] = $name;
+            foreach (static::labels() as $key => $val) {
+                $result[$key] = $val;
+            }
+            return $result;
+        } else {
+            return static::labels();
+        }
+    }
+
+    public static function label($val)
+    {
+        return static::labels()[$val] ?? $val;
+    }
+
+    public static function exists($va): bool
+    {
+        return isset(static::labels()[$va]);
+    }
+
+}
