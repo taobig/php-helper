c79e9e6982cc2b70c4a31420b45edf23acf18763
taobig <studentht@hotmail.com>
2020-12-17 08:39:27 +0800

    Remove YiiRedisConnection class

--- tests/helpers/lock/RedisLockTest.php
+++ tests/helpers/lock/RedisLockTest.php
@@ -4,8 +4,6 @@
 use taobig\helpers\lock\exceptions\LockFailedException;
 use taobig\helpers\lock\RedisExtensionConnection;
 use taobig\helpers\lock\RedisLock;
-use taobig\helpers\lock\YiiRedisConnection;
-use yii\redis\Connection;
 
 class RedisLockTest extends TestCase
 {
@@ -35,47 +33,6 @@ class RedisLockTest extends TestCase
         $redis = new \Redis();
         $redis->connect("localhost");
         $redisConnection = new RedisExtensionConnection($redis);
-        foreach ([true, false] as $enableEvalCommand) {
-            $redisLock = new RedisLock($redisConnection, '1', $enableEvalCommand);
-
-            $lockedKey = "testtesttest_" . time();;
-            $lockedValue = $redisLock->lock($lockedKey, 200);
-            $this->assertSame(true, is_int($lockedValue));
-
-            $redisLock->unlock($lockedKey, $lockedValue);
-
-            $redisLock->lock($lockedKey, 200);
-        }
-    }
-
-    public function testLockWithYiiRedisConnection()
-    {
-        require_once __DIR__ . '/../../../vendor/yiisoft/yii2/Yii.php';
-
-        $redis = new Connection();
-        $redisConnection = new YiiRedisConnection($redis);
-        foreach ([true, false] as $enableEvalCommand) {
-            $redisLock = new RedisLock($redisConnection, '1', $enableEvalCommand);
-
-            $this->assertSame(RedisLock::class, $redisLock->name());
-            $this->assertSame(RedisLock::class, $redisLock->getName());
-
-            $lockedKey = "testtesttest_" . time();
-            $lockedValue = $redisLock->lock($lockedKey, 200);
-            $this->assertSame(true, is_int($lockedValue));
-
-            $this->expectException(LockFailedException::class);
-            $redisLock->lock($lockedKey, 200);
-        }
-    }
-
-
-    public function testUnlockWithYiiRedisConnection()
-    {
-        require_once __DIR__ . '/../../../vendor/yiisoft/yii2/Yii.php';
-
-        $redis = new Connection();
-        $redisConnection = new YiiRedisConnection($redis);
         foreach ([true, false] as $enableEvalCommand) {
             $redisLock = new RedisLock($redisConnection, '1', $enableEvalCommand);
 
