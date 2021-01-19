86469465777d91b5b81f6e1ef7b8c9a482b397f1
taobig <studentht@hotmail.com>
2021-01-19 20:36:35 +0800

    error_handler支持@操作符

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -12,9 +12,16 @@ class ErrorHandler
 //        error_reporting(0);//将会关闭所有的错误
         ini_set("display_errors", 0);
 
-        set_error_handler(function ($errno, $str, $file, $line) {
+        set_error_handler(function (int $errno, string $str, string $file = '', int $line = 0) {
+            //when @ is used
+//            if (error_reporting() == 0) {//< PHP-8.0.0, error_reporting()=0
+            if (!(error_reporting() & $errno)) {//>= PHP-8.0.0, error_reporting()>0
+                return false; //Silenced
+            }
             throw new \ErrorException($str, 0, $errno, $file, $line);
         });
+//        @trigger_error("@ operator");
+//        trigger_error("Hello");
 //        set_exception_handler(function (\Throwable $exception) {});
     }
 }
