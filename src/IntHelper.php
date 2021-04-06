7fb3fcce71716fd829cc20e344b6a9216a26285e
taobig <studentht@hotmail.com>
2021-04-06 20:12:48 +0800

    支持科学计数法表示的数字

--- src/IntHelper.php
+++ src/IntHelper.php
@@ -6,14 +6,19 @@ class IntHelper
 {
 
     /**
-     * 不支持检测科学计数法格式的正整数
-     * @param string|int $val
+     * 1. 保持使用当前运行时默认的浮点数精度：https://www.php.net/manual/en/language.types.float.php
+     * 2. 不支持大于PHP_INT_MAX（有符号的最大整数值）的无符号整数值
+     * @param string|int|float $val
      * @return bool
      */
     public static function isUnsignedInt($val): bool
     {
         if (is_int($val)) {
             return $val > 0;
+        } else if (is_float($val)) {
+            if (ctype_digit(strval($val))) {
+                return $val > 0;
+            }
         } else if (is_string($val)) {
             if (preg_match('/^\d+$/', $val)) {
                 return true;
--- tests/helpers/IntHelperTest.php
+++ tests/helpers/IntHelperTest.php
@@ -8,16 +8,27 @@ use TestCase;
 class IntHelperTest extends TestCase
 {
 
-    public function testStartsWith()
+    public function testIsUnsignedInt()
     {
+        $this->assertSame(true, IntHelper::isUnsignedInt(111));
+        $this->assertSame(true, IntHelper::isUnsignedInt(111_222_3));//as of PHP 7.4.0
+
         $this->assertSame(false, IntHelper::isUnsignedInt("hello world"));
         $this->assertSame(false, IntHelper::isUnsignedInt("111 2222"));
         $this->assertSame(true, IntHelper::isUnsignedInt("111"));
+        $this->assertSame(false, IntHelper::isUnsignedInt("111 "));
+        $this->assertSame(false, IntHelper::isUnsignedInt(" 111"));
         $this->assertSame(false, IntHelper::isUnsignedInt("111.11"));
         $this->assertSame(true, IntHelper::isUnsignedInt("0"));
         $this->assertSame(false, IntHelper::isUnsignedInt("1.1111111111111E+28"));
-        $this->assertSame(false, IntHelper::isUnsignedInt("1.111"));
+        $this->assertSame(true, IntHelper::isUnsignedInt(1E+8));
+        $this->assertSame(true, IntHelper::isUnsignedInt(1E+10));
+        $this->assertSame(false, IntHelper::isUnsignedInt(1E+18));
+        $this->assertSame(true, IntHelper::isUnsignedInt(1.00));
+        $this->assertSame(true, IntHelper::isUnsignedInt(floatval(2)));
         $this->assertSame(false, IntHelper::isUnsignedInt(1.111));//float
+
+        $this->assertSame(false, IntHelper::isUnsignedInt(PHP_INT_MAX + 1));
     }
 
 
