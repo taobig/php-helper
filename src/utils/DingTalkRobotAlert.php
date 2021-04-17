3cfd03cfd5bb2388e987d722b46cf419e4cef317
taobig <studentht@hotmail.com>
2021-04-17 21:39:11 +0800

    钉钉机器人支持加签的场景

--- src/utils/DingTalkRobotAlert.php
+++ src/utils/DingTalkRobotAlert.php
@@ -12,10 +12,12 @@ use taobig\helpers\http\HttpRequest;
 class DingTalkRobotAlert
 {
     private string $url;
+    private string $secret;
 
-    public function __construct(string $url)
+    public function __construct(string $url, string $secret = '')
     {
         $this->url = $url;
+        $this->secret = $secret;
     }
 
     //{
@@ -42,8 +44,27 @@ class DingTalkRobotAlert
      */
     public function alertText(string $message, array $atMobiles = [], bool $isAtAll = false)
     {
+        $url = $this->url;
+        $secret = $this->secret;
+        if ($secret) {
+            //timestamp+"\n"+密钥当做签名字符串
+            $timestamp = time() * 1000;//当前时间戳，单位是毫秒，与请求调用时间误差不能超过1小时。
+            $content = sprintf("%d\n%s", $timestamp, $secret);
+            $str = hash_hmac('sha256', $content, $secret, true);
+            $sign = urlencode(base64_encode($str));
+
+            $params = [
+                'timestamp' => $timestamp,
+                'sign' => $sign,
+            ];
+            if (strpos($url, '?') === false) {
+                $url = $url . '?' . http_build_query($params);
+            } else {
+                $url = $url . '&' . http_build_query($params);
+            }
+        }
         $httpClient = new HttpRequest();
-        $result = $httpClient->postJson($this->url, [
+        $result = $httpClient->postJson($url, [
             'msgtype' => 'text',
             "text" => [
                 "content" => $message,//如果@后的手机号在atMobiles里有，那么消息体里@手机号就是占位符会被显示为@+昵称； 否则就会被当做文本输出。
