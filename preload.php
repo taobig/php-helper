02ec5ef9b5187afb878ade75d86995ac124ac49c
taobig <studentht@hotmail.com>
2021-12-11 21:04:16 +0800

    Composer require symfony/polyfill-php81

--- preload.php
+++ preload.php
@@ -1,12 +1,13 @@
 <?php
 
-// PHP 8.1: New array_is_list function
-if (!function_exists('array_is_list')) {
-    function array_is_list(array $arr): bool
-    {
-        if (empty($arr)) {
-            return true;
-        }
-        return array_keys($arr) === range(0, count($arr) - 1);
-    }
-}
+// @see symfony/polyfill-php81
+//// PHP 8.1: New array_is_list function
+//if (!function_exists('array_is_list')) {
+//    function array_is_list(array $arr): bool
+//    {
+//        if (empty($arr)) {
+//            return true;
+//        }
+//        return array_keys($arr) === range(0, count($arr) - 1);
+//    }
+//}
