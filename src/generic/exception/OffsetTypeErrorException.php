768000d8d63a9306d8a16acb8c04700bc6e99c6d
taobig <studentht@hotmail.com>
2021-12-11 20:52:49 +0800

    Remove generic\ArrayList class and generic\HashMap class (Compatible with PHP8.1)

--- src/generic/exception/OffsetTypeErrorException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace taobig\helpers\generic\exception;
-
-use taobig\helpers\base\BaseException;
-
-class OffsetTypeErrorException extends BaseException {
-
-}
--- src/generic/exception/UnsupportedOperationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace taobig\helpers\generic\exception;
-
-use taobig\helpers\base\BaseException;
-
-class UnsupportedOperationException extends BaseException {
-
-}
