37a2d9b1316159330a9e3416f0f3bdef330c682d
taobig <studentht@hotmail.com>
2021-12-11 19:45:23 +0800

    Fix psalm error

--- src/NumberHelper.php
+++ src/NumberHelper.php
@@ -7,15 +7,12 @@ class NumberHelper
 
     /**
      *
-     * @param int $n 0 <= $n <= 9
+     * @param int $n 0 <= n <= 9, if n<0 or n>9, return ''
      * @return string
      */
     public static function singleDigitToSimpleChinese(int $n): string
     {
-        if ($n < 0 || $n > 9) {
-            return '';
-        }
-        return ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'][$n];
+        return (['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'][$n]) ?? '';
     }
 
 }
