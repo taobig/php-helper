6439f3889224625afd4df84ca71a63eb84b470f4
taobig <studentht@hotmail.com>
2021-12-11 19:17:43 +0800

    Add RandomHelper::str(), Deprecated RandomHelper::getRandomEnglishCharacters()

--- src/RandomHelper.php
+++ src/RandomHelper.php
@@ -5,14 +5,27 @@ namespace taobig\helpers;
 class RandomHelper
 {
 
+    /**
+     * @deprecated
+     */
     const CASE_LOWER = 0;
+    /**
+     * @deprecated
+     */
     const CASE_UPPER = 1;
+
+    public const DIGITS = 1 << 0;//bin:0001
+    public const LOWER_CASE_ALPHABET = 1 << 1;//bin:0010
+    public const UPPER_CASE_ALPHABET = 1 << 2;//bin:0100
+
 
     /**
      * @param int $characterCount
      * @param int $caseFlag
      * @return string
-     * @internal param bool $characterCanRepeat
+     * @throws \Exception
+     * @see StringHelper::random()
+     * @deprecated
      */
     public function getRandomEnglishCharacters(int $characterCount, int $caseFlag = self::CASE_LOWER)
     {
@@ -29,6 +42,37 @@ class RandomHelper
             $str .= chr($iVal);
         }
 
+        return $str;
+    }
+
+
+    /**
+     * 生成一个可选包含大写英文字母、小写英文字母、数字的字符串
+     * @param int $length
+     * @param int $numType eg. self::DIGITS,self::LOWER_CASE_ALPHABET,self::UPPER_CASE_ALPHABET,self::DIGITS|self::LOWER_CASE_ALPHABET,...
+     * @return string
+     */
+    public static function str(int $length, int $numType): string
+    {
+        $arr = [];
+        if ($numType & self::DIGITS) {
+            $arr = array_merge($arr, range(0, 9));
+        }
+        if ($numType & self::LOWER_CASE_ALPHABET) {
+            $arr = array_merge($arr, range('a', 'z'));
+        }
+        if ($numType & self::UPPER_CASE_ALPHABET) {
+            $arr = array_merge($arr, range('A', 'Z'));
+        }
+        if (empty($arr)) {
+            throw new \InvalidArgumentException();
+        }
+        $str = '';
+        $arr_len = count($arr);
+        for ($i = 0; $i < $length; $i++) {
+            $rand = mt_rand(0, $arr_len - 1);
+            $str .= $arr[$rand];
+        }
         return $str;
     }
 
--- tests/helpers/RandomHelperTest.php
+++ tests/helpers/RandomHelperTest.php
@@ -39,4 +39,48 @@ class RandomHelperTest extends \TestCase
         }
     }
 
+
+    public function testStr()
+    {
+        $char = RandomHelper::str(1, RandomHelper::LOWER_CASE_ALPHABET);
+        $this->assertGreaterThanOrEqual(97, ord($char));
+        $this->assertLessThanOrEqual(122, ord($char));
+
+        $char = RandomHelper::str(1, RandomHelper::UPPER_CASE_ALPHABET);
+        $this->assertGreaterThanOrEqual(65, ord($char));
+        $this->assertLessThanOrEqual(90, ord($char));
+
+        $count = 50;
+        $chars = RandomHelper::str($count, RandomHelper::LOWER_CASE_ALPHABET);
+        $this->assertSame($count, strlen($chars));
+        foreach (str_split($chars) as $_char) {
+            $this->assertGreaterThanOrEqual(97, ord($_char));
+            $this->assertLessThanOrEqual(122, ord($_char));
+        }
+        $this->assertMatchesRegularExpression("/^[a-z]{{$count}}$/", $chars);
+
+        $chars = RandomHelper::str($count, RandomHelper::UPPER_CASE_ALPHABET);
+        $this->assertSame($count, strlen($chars));
+        foreach (str_split($chars) as $_char) {
+            $this->assertGreaterThanOrEqual(65, ord($_char));
+            $this->assertLessThanOrEqual(90, ord($_char));
+        }
+        $this->assertMatchesRegularExpression("/^[A-Z]{{$count}}$/", $chars);
+
+        $chars = RandomHelper::str($count, RandomHelper::DIGITS);
+        $this->assertSame($count, strlen($chars));
+        foreach (str_split($chars) as $_char) {
+            $this->assertGreaterThanOrEqual(0, (int)($_char));
+            $this->assertLessThanOrEqual(9, (int)($_char));
+        }
+        $this->assertMatchesRegularExpression("/^[0-9]{{$count}}$/", $chars);
+
+        $chars = RandomHelper::str($count, RandomHelper::LOWER_CASE_ALPHABET | RandomHelper::UPPER_CASE_ALPHABET | RandomHelper::DIGITS);
+        $this->assertSame($count, strlen($chars));
+        $this->assertMatchesRegularExpression("/^[0-9a-zA-Z]{{$count}}$/", $chars);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $chars = RandomHelper::str($count, 0);
+    }
+
 }
